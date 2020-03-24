f41bf64f0ee01aafc760c6c08694f764ff4c25c7
Rakshay Kanthadai <rsk146@ilab1.cs.rutgers.edu>
2020-03-24 17:46:38 -0400

    flags check

--- Huffman.c
+++ Huffman.c
@@ -2,28 +2,29 @@
 #include <stdlib.h>
 #include <string.h>
 
-void flagCheck(int flag);
+void flagCheck(int flag, int argc, char* argv[]);
 
 int main(int argc, char* argv[]){
-    flagCheck(1);
+    flagCheck(1, argc, argv);
     return 0;
 }
 
 //checks the first 2 flags to see what we're doing and if it's recursive or not
-void flagCheck(int pos){
-    if(*(argv[pos]) == "-"){
-        if(*(argv[pos]+1) == "b"){
-            printf("Using Build Huffman Codebook Flag");
+void flagCheck(int pos, int argc, char* argv[]){
+    if(*argv[pos] == '-'){
+        if(*(argv[pos]+1) == 'b'){
+            printf("Using Build Huffman Codebook Flag\n");
         }
-        else if(*(argv[pos]+1) == "c"){
-            printf("Using Compression Flag")
+        else if(*(argv[pos]+1) == 'c'){
+            printf("Using Compression Flag\n");
         }
-        else if(*(argv[pos]+1) == "d"){
-            printf("Using the Decompression Flag");
+        else if(*(argv[pos]+1) == 'd'){
+            printf("Using the Decompression Flag\n");
         }
-        else if(*(argv[pos]+1) == "R"){
-            printf("Using the Recursion Flag");
-            if(pos == 1) flagCheck(pos+1);
+        else if(*(argv[pos]+1) == 'R'){
+            printf("Using the Recursion Flag\n");
         }
-    }
+    }               
+    flagCheck(pos+1, argc, argv);
+
 }
