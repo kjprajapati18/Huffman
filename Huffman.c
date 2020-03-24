c826b12fb5015144b43e9d324a7e8c06b90a5256
Rakshay Kanthadai <rsk146@ilab1.cs.rutgers.edu>
2020-03-24 16:55:30 -0400

    first flags check

--- Huffman.c
+++ Huffman.c
@@ -2,8 +2,28 @@
 #include <stdlib.h>
 #include <string.h>
 
-int main(int argv, char* argc[]){
-    printf("hello\n");
-    printf("hello wordl");
+void flagCheck(int flag);
+
+int main(int argc, char* argv[]){
+    flagCheck(1);
     return 0;
+}
+
+//checks the first 2 flags to see what we're doing and if it's recursive or not
+void flagCheck(int pos){
+    if(*(argv[pos]) == "-"){
+        if(*(argv[pos]+1) == "b"){
+            printf("Using Build Huffman Codebook Flag");
+        }
+        else if(*(argv[pos]+1) == "c"){
+            printf("Using Compression Flag")
+        }
+        else if(*(argv[pos]+1) == "d"){
+            printf("Using the Decompression Flag");
+        }
+        else if(*(argv[pos]+1) == "R"){
+            printf("Using the Recursion Flag");
+            if(pos == 1) flagCheck(pos+1);
+        }
+    }
 }
