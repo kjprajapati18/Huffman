87bc1e3748ffbf852309f2394cea8850285244ed
Krishna Prajapati <kjprajapati18@users.noreply.github.com>
2020-04-05 16:19:51 -0400

    Moved functions back to correct files

--- codebookWriter.c
+++ codebookWriter.c
@@ -5,7 +5,7 @@
 #include <unistd.h>
 #include "codebookWriter.h"
 
-/*int writeCodebook(treeNode* head, int fd, char* escapeChar, char* bitString){
+int writeCodebook(treeNode* head, int fd, char* escapeChar, char* bitString){
     if(head == NULL) return -1;
     int bitLength = strlen(bitString);
 
@@ -15,7 +15,7 @@
         int size = strlen(escapeChar);
         
         if(booleanIsSpace){
-            temp = (char*) malloc((bitLength+size+3)*sizeof(char));    //bitLength(for code) + 3 (for null-term, tab, and newline) + strlen(for token)
+            temp = (char*) malloc((bitLength+size+4)*sizeof(char));    //bitLength(for code) + 3 (for null-term, tab, and newline) + strlen(for token)
         } else {
             temp = (char*) malloc((bitLength+strlen(head->token)+3)*sizeof(char));
         }    
@@ -43,7 +43,7 @@
                 break;
             default:
                 break;
-        }
+        }*/
         
         char* inputtedToken = booleanIsSpace? escapeCharHandler(escapeChar, head->token) : head->token;
 
@@ -71,7 +71,7 @@
 
     free(newString);
     return 0;
-}*/
+}
 
 int writeString(int fd, char* string){
 
